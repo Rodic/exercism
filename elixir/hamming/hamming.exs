bfbdadae732dc2b445d483cf7c0d3f8b9c8dfbce
rodic <alec.rodic@gmail.com>
2016-05-20 14:06:02 +0200

    fixing guard syle

--- elixir/hamming/hamming.exs
+++ elixir/hamming/hamming.exs
@@ -8,7 +8,9 @@ defmodule DNA do
   4
   """
   @spec hamming_distance([char], [char]) :: non_neg_integer
-  def hamming_distance(str1, str2) when length(str1) != length(str2), do: nil
+  def hamming_distance(str1, str2)
+    when length(str1) != length(str2),
+    do: nil
 
   def hamming_distance(strand1, strand2) do
     Stream.zip(strand1, strand2)
